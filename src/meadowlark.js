1b707b6e8c14c5c23d2147c6b7d56d5a809aa9c0
Damian Temimi <damian.temimi@gmail.com>
2024-10-10 23:33:35 +0200

    wip

--- src/meadowlark.js
+++ src/meadowlark.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
-
+const path = require("path"); // Importowanie modułu path dla bezpiecznej obsługi ścieżek
+const fortune = require("../lib/fortune");
+const handlers = require("../lib/handlers");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,48 +10,41 @@ const port = process.env.PORT || 3000;
 app.engine(
   "hbs",
   engine({
-    layoutsDir: __dirname + "/views/layouts", // Ścieżka do układów (layouts)
+    layoutsDir: path.join(__dirname, "../views/layouts"), // Ścieżka do układów (layouts)
     extname: ".hbs", // Ustawienie rozszerzenia na .hbs
   })
 );
 app.set("view engine", "hbs");
-app.set("views", __dirname + "/views"); // Ścieżka do folderu z widokami
+app.set("views", path.join(__dirname, "../views")); // Ścieżka do folderu z widokami
 
-app.use(express.static(__dirname + "/public"));
+// Ścieżka do katalogu public
+app.use(express.static(path.join(__dirname, "../public"))); // Poprawiona ścieżka do katalogu public
 
 // Definiowanie trasy głównej
-app.get("/", (req, res) => res.render("home", { layout: "main" }));
+app.get("/", handlers.home);
 
 // Definiowanie trasy /about
-app.get("/about", (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render("about", { fortune: randomFortune });
-});
+app.get("/about", handlers.about);
 
 // Obsługa błędu 404
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-});
+app.use(handlers.notFound);
 
 // Obsługa błędu 500
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render("500");
-});
+app.use(handlers.serverError);
 
 // Uruchomienie serwera
-app.listen(port, () =>
-  console.log(
-    `Express started at: http://localhost:${port}; press Ctrl-C to exit`
-  )
-);
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(
+      `Express został uruchomiony pod adresem http://localhost:${port}` +
+        "; naciśnij Ctrl-C, aby zakończyć."
+    );
+  });
+} else {
+  module.exports = app;
+}
 
-const fortunes = [
-  "Pokonaj swoje lęki albo one pokonają Ciebie.",
-  "Rzeki potrzebują źródeł",
-  "Nie obawiaj się nieznanego.",
-  "Oczekuj przyjemnej niespodzianki.",
-  "Zawsze szukaj prostego rozwiązania.",
-];
+// Dodanie prostego logu ścieżki za pomocą __dirname
+setTimeout(() => {
+  console.log(__dirname);
+}, 2000);
