e33894723a1d2d388f1821d82cf48d89a143f241
Damian Temimi <damian.temimi@gmail.com>
2024-10-12 23:04:06 +0200

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      semi: ['error', 'always'],
+    },
+  },
 ];
+
--- src/meadowlark.js
+++ src/meadowlark.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
-const path = require('path'); // Importowanie modułu path dla bezpiecznej obsługi ścieżek
-const fortune = require('../lib/fortune');
+const path = require('path');
 const handlers = require('../lib/handlers');
 const app = express();
 const port = process.env.PORT || 3000;
+const weatherMiddleware = require('../lib/middleware/weather');
+
 // Konfiguracja silnika widoków Handlebars
 app.engine(
   'hbs',
   engine({
-    layoutsDir: path.join(__dirname, '../views/layouts'), // Ścieżka do układów (layouts)
-    extname: '.hbs', // Ustawienie rozszerzenia na .hbs
+    layoutsDir: path.join(__dirname, '../views/layouts'),
+    extname: '.hbs',
+    defaultLayout: 'main',
+    helpers: {
+      section: function (name, options) {
+        if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+      },
+    },
   })
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '../views')); // Ścieżka do folderu z widokami
+app.set('views', path.join(__dirname, '../views'));
 
 // Ścieżka do katalogu public
-app.use(express.static(path.join(__dirname, '../public'))); // Poprawiona ścieżka do katalogu public
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(weatherMiddleware);
 
 // Definiowanie trasy głównej
 app.get('/', handlers.home);
+
+app.get('/test', (req, res) => {
+  res.render('section-test');
+});
 
 // Definiowanie trasy /about
 app.get('/about', handlers.about);
